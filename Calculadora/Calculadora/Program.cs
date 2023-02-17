e47676d2abc37c475de3541df60ecff8dfe9177a
Antonio Willian <antoniowillian075@gmail.com>
2023-02-16 22:21:19 -0300

    Criação de uma simples calculadora

---
+++ Calculadora/Calculadora/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite o primeiro valor: ");
+
+double num1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo valor: ");
+
+double num2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Escolha o operador: ");
+
+string operador = Console.ReadLine();
+
+switch (operador)
+{
+    case "+":
+        Console.WriteLine(num1 + num2);
+        break;
+    case "-":
+        Console.WriteLine(num1 - num2);
+        break;
+    case "*":
+        Console.WriteLine(num1 * num2);
+        break;
+    case "/":
+        Console.WriteLine(num1 / num2);
+        break;
+    default:
+        Console.WriteLine("Valor não identificado");
+        break;
+}
