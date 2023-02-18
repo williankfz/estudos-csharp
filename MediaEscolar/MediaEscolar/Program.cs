ba5c38b4f55879a915f3b658a4f8adfe6bf203e9
Antonio Willian <antoniowillian075@gmail.com>
2023-02-18 12:20:50 -0300

    Projeto de média escolar de 3 notas

---
+++ MediaEscolar/MediaEscolar/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Digite a primeira nota: ");
+int nota1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a segunda nota: ");
+int nota2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite a terceira nota: ");
+int nota3 = int.Parse(Console.ReadLine());
+
+int media = (nota1 + nota2 + nota3) / 3;
+
+if (media >= 6)
+{
+    Console.WriteLine("Sua média é: " + media + " Você passou.");
+}else if(media >=4 && media < 6)
+{
+    Console.WriteLine("Sua média é: " + media + " Você está de recuperação.");
+}
+else
+{
+    Console.WriteLine("Sua média é: " + media + " Você está reprovado.");
+}
+
+
