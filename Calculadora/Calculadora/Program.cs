cba8d49c4da31b8c9cf278e7e6d3af82cc613bf3
Antonio Willian <antoniowillian075@gmail.com>
2023-02-24 21:53:21 -0300

    Alterando para utilizar Class

--- Calculadora/Calculadora/Program.cs
+++ Calculadora/Calculadora/Program.cs
@@ -1,29 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using Calculadora;
+
+Valores num1, num2, operador;
+num1 = new Valores();
+num2 = new Valores();
+operador = new Valores();
+
 Console.WriteLine("Digite o primeiro valor: ");
 
-double num1 = double.Parse(Console.ReadLine());
+num1.valor = double.Parse(Console.ReadLine());
 
 Console.WriteLine("Digite o segundo valor: ");
 
-double num2 = double.Parse(Console.ReadLine());
+num2.valor = double.Parse(Console.ReadLine());
 
 Console.WriteLine("Escolha o operador: ");
 
-string operador = Console.ReadLine();
+operador.operador = Console.ReadLine();
 
-switch (operador)
+switch (operador.operador)
 {
     case "+":
-        Console.WriteLine(num1 + num2);
+        Console.WriteLine(num1.valor + num2.valor);
         break;
     case "-":
-        Console.WriteLine(num1 - num2);
+        Console.WriteLine(num1.valor - num2.valor);
         break;
     case "*":
-        Console.WriteLine(num1 * num2);
+        Console.WriteLine(num1.valor * num2.valor);
         break;
     case "/":
-        Console.WriteLine(num1 / num2);
+        Console.WriteLine(num1.valor / num2.valor);
         break;
     default:
         Console.WriteLine("Valor não identificado");
