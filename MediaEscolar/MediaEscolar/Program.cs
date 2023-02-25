b3bb0af8f187a4b3af9632a4f13cbab850a2fc49
Antonio Willian <antoniowillian075@gmail.com>
2023-02-24 22:10:26 -0300

    Utilizando Class

--- MediaEscolar/MediaEscolar/Program.cs
+++ MediaEscolar/MediaEscolar/Program.cs
@@ -1,26 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using MediaEscolar;
+
+Notas nota1, nota2,nota3, media;
+nota1 = new Notas();
+nota2 = new Notas();
+nota3 = new Notas();
+media = new Notas();
+
 Console.WriteLine("Digite a primeira nota: ");
-int nota1 = int.Parse(Console.ReadLine());
+nota1.nota = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Digite a segunda nota: ");
-int nota2 = int.Parse(Console.ReadLine());
+nota2.nota = int.Parse(Console.ReadLine());
 
 Console.WriteLine("Digite a terceira nota: ");
-int nota3 = int.Parse(Console.ReadLine());
+nota3.nota = int.Parse(Console.ReadLine());
 
-int media = (nota1 + nota2 + nota3) / 3;
+media.media = (nota1.nota + nota2.nota + nota3.nota) / 3;
+int resultadoFinal = media.media;
 
-if (media >= 6)
+if (resultadoFinal >= 6)
 {
-    Console.WriteLine("Sua média é: " + media + " Você passou.");
-}else if(media >=4 && media < 6)
+    Console.WriteLine("Sua média é: " + resultadoFinal + " Você passou.");
+}else if(resultadoFinal >= 4 && resultadoFinal < 6)
 {
-    Console.WriteLine("Sua média é: " + media + " Você está de recuperação.");
+    Console.WriteLine("Sua média é: " + resultadoFinal + " Você está de recuperação.");
 }
 else
 {
-    Console.WriteLine("Sua média é: " + media + " Você está reprovado.");
+    Console.WriteLine("Sua média é: " + resultadoFinal + " Você está reprovado.");
 }
 
 
