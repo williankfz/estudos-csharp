9d6442438b3633699dc72a861937ed854b315d0b
Antonio Willian <antoniowillian075@gmail.com>
2023-02-25 22:41:13 -0300

    Inclusão de EstoqueProdutoExercicio

---
+++ EstoqueProdutoExercicio/EstoqueProdutoExercicio/Produto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace EstoqueProdutoExercicio
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public double Total()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void Adicionar(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void Remover(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", $" + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + Quantidade + " unidades, Total: $" + Total().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ EstoqueProdutoExercicio/EstoqueProdutoExercicio/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EstoqueProdutoExercicio;
+using System.Globalization;
+
+Produto p = new Produto();
+
+Console.WriteLine("Entre com os dados do produto: ");
+
+Console.Write("Nome: ");
+p.Nome = Console.ReadLine();
+
+Console.Write("Preço: ");
+p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write("Quantidade: ");
+p.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.WriteLine(p);
+
+Console.WriteLine();
+
+Console.Write("Aumente o numero de produtos no estoque: ");
+int qte = int.Parse(Console.ReadLine());
+p.Adicionar(qte);
+
+Console.WriteLine(p);
+
+Console.WriteLine();
+
+Console.Write("Diminua o numero de produtos no estoque: ");
+qte = int.Parse(Console.ReadLine());
+p.Remover(qte);
+
+Console.WriteLine("O valor final do estoque é: " + p);
