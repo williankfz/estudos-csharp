28fdd94bb8cfee3bbdac4644336930700401f8bc
Antonio Willian <antoniowillian075@gmail.com>
2023-02-26 13:21:39 -0300

    Inclusão de exercicio sobre static

---
+++ CotacaoDolar/CotacaoDolar/ConversorDeMoeda.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace CotacaoDolar
+{
+    internal class ConversorDeMoeda
+    {
+        public static double iof = 6.0;
+
+        public static double Calculo(double quantia, double cotacao)
+        {
+            double total = quantia * cotacao;
+            return total + total * iof / 100.0;
+        }
+    }
+}
---
+++ CotacaoDolar/CotacaoDolar/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CotacaoDolar;
+using System.Globalization;
+
+
+Console.Write("Qual é a cotação do dolar? ");
+double cotacao = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+Console.Write("Quantos dolares você vai comprar? ");
+double quantia = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+double result = ConversorDeMoeda.Calculo(cotacao, quantia);
+
+Console.WriteLine("Valor a ser pago em reais = " + result.ToString("F2",CultureInfo.InvariantCulture));
