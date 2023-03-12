683a9be03a8962782b56296a6e3bb44d2cc82ee6
Antonio Willian <antoniowillian075@gmail.com>
2023-03-11 23:06:32 -0300

    Inclusão de segundo exercicio sobre composições

---
+++ ComposicaoExercicio2/ComposicaoExercicio2/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComposicaoExercicio2.Entities.Enums
+{
+    internal enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3,
+    }
+}
